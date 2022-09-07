81b1217106234e9d2901af341a0bcc1d6c19d16d
Sazzad-Mannan <sazzadmannan16@gmail.com>
2022-09-07 21:54:24 +0600

    c

--- family.pl
+++ family.pl
@@ -67,14 +67,23 @@ parant('Shopna','Inaya').
 parant('Forida','Tohid').
 parant('Forida','Sohid').
 parant('Forida','Rasel').
+parant('Mazhar','Tohid').
+parant('Mazhar','Sohid').
+parant('Mazhar','Rasel').
 
 parant('Rashu','Shetu').
 parant('Rashu','Sajid').
 parant('Rashu','Ziyad').
+parant('Aktar','Shetu').
+parant('Aktar','Sajid').
+parant('Aktar','Ziyad').
 
 parant('Kushi','Moon').
 parant('Kushi','Mim').
 parant('Kushi','Eti').
+parant('Yunus','Moon').
+parant('Yunus','Mim').
+parant('Yunus','Eti').
 
 parant('Harun','Saifullah').
 parant('Harun','Aminullah').
@@ -106,6 +115,8 @@ parant('AbulBashar','Ruksar').
 parant('AbulBashar','Tumpa').
 
 
+
+
 male('Mokbul').
 male('Ahmed').
 male('Abdul').
@@ -135,6 +146,9 @@ male('Rasel').
 male('Ziyad').
 male('Sajid').
 male('Sohid').
+male('Aktar').
+male('Yunus').
+male('Mazhar').
 
 
 female('Rabeya').
@@ -178,7 +192,7 @@ dadi(X,Z):- parant(X,Y),parant(Y,Z),male(Y),female(X).
 nana(X,Z):- parant(X,Y),parant(Y,Z),female(Y),male(X).
 nani(X,Z):- parant(X,Y),parant(Y,Z),female(Y),female(X).
 chacha(X,Z):- parant(F,Z),siblings(F,X),male(F),male(X).
-chachi(X,Z):- chachatobhai(Y,Z),mother(X,Y).
+chachi(X,Z):- chacha(Y,Z),husband(Y,X).
 fufu(X,Z):- parant(F,Z),siblings(F,X),male(F),female(X).
 mama(X,Z):- parant(F,Z),siblings(F,X),female(F),male(X).
 khala(X,Z):- parant(F,Z),siblings(F,X),female(F),female(X).
@@ -190,3 +204,4 @@ mamatobhai(X,Z):-parant(M,X),parant(N,Z),siblings(M,N),male(M),female(N),male(X)
 mamatobon(X,Z):-parant(M,X),parant(N,Z),siblings(M,N),male(M),female(N),female(X).
 khalatobhai(X,Z):-parant(M,X),parant(N,Z),siblings(M,N),female(M),female(N),male(X).
 khalatobon(X,Z):-parant(M,X),parant(N,Z),siblings(M,N),female(M),female(N),female(X).
+husband(X,Y):- father(X,Z),mother(Y,Z).
